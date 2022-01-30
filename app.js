4c5a32df2bb57d0ba0d6969811834868ec7c0c0f
Kamble, Nikhil <nikrox98@gmail.com>
2022-01-30 00:48:59 -0500

    working mongo db

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const express = require('express');
-const app = express();
-const port = 8080;
+require('dotenv').config()
 
-app.use(express.static(__dirname+'/'));
+const express = require('express')
+const app = express()
+const port = 3000
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to MongoDB on port '+ db.port))
+
+app.use(express.json())
+
+const usersRouter = require('./routes/users')
+app.use('/users', usersRouter)
 
 app.get('/', function (req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, '127.0.0.1', function(error){
-    if(error){
-        console.log('Something went wrong', error)
-    }
-    else{
-        console.log('Server is listening on port ' + port)
-    }
-})
+app.listen(port, () =>  console.log('Server is listening on port ' + port))
