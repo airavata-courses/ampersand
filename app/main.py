492c06fdda529abfb73e4ff56795ed70530737ec
dhruti-patel <pateldhruti.dp@gmail.com>
2022-01-31 20:07:00 -0500

    dockerized service

--- app/main.py
+++ app/main.py
@@ -22,7 +22,8 @@ def plot_radar():
                 vmin=-32, vmax=64, colorbar_label='', ax=ax)
     display.plot_range_ring(radar.range['data'][-1]/1000., ax=ax)
     display.set_limits(xlim=(-500, 500), ylim=(-500, 500), ax=ax)
-    plt.show()
+    #plt.show()
+    plt.savefig("test.png")
 
 class userURL(BaseModel):
     user_id : str
