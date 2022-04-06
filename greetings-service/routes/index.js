335ba09b8d629d7e48c82e7aa3d3b9d2ec48e304
Dhruti Patel <pateldhruti.dp@gmail.com>
2022-04-06 17:00:18 +0000

    data-ingestor deployment change

--- greetings-service/routes/index.js
+++ greetings-service/routes/index.js
@@ -13,6 +13,10 @@ router.get('/greetme', (req, res, next) => {
      res.status(500).send('An error has occurred, we cannot greet you at the moment.');
    }
    else {
+     console.log(user)
+     console.log(user.name)
+     console.log(user.name.givenName)
+     console.log(user.name.familyName)
      res.send({ success: true, fullName: `${user.name.givenName} ${user.name.familyName}` })
    }
  })(req, res, next);
--- gui/src/components/PostForm.js
+++ gui/src/components/PostForm.js
@@ -12,7 +12,7 @@ function PostForm(){
     document.title = "Dashboard";
     var r_radar = ""
 
-    const [name, setName] = useState('');
+    const [name, setName] = useState('Guest User');
 
     useEffect(async () => {
         // gateway call for username
