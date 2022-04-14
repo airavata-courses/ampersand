fc8b79f37708941a96030bcc15fe8a236519fc3a
Kamble, Nikhil <nikrox98@gmail.com>
2022-04-14 00:19:10 -0400

    ip changes

--- account-service/Utilities.js
+++ account-service/Utilities.js
@@ -1,4 +1,4 @@
-const host_url = 'http://localhost';
-// const host_url = 'http://149.165.154.175';
+// const host_url = 'http://localhost';
+const host_url = 'http://149.165.154.175';
 // export default { host_url }
 module.exports = {host_url};
