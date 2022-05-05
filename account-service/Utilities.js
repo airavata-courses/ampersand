7bed8fc4accd35a755e5b0b1b63f284cdc50bf24
dhruti-patel <pateldhruti.dp@gmail.com>
2022-05-05 18:39:32 -0400

    initial custos commit

--- account-service/Utilities.js
+++
@@ -1,4 +0,0 @@
-// const host_url = 'http://localhost';
-const host_url = 'http://149.165.154.175';
-// export default { host_url }
-module.exports = {host_url};
--- greetings-service/Utilities.js
+++
@@ -1,3 +0,0 @@
-// const host_url = 'http://localhost';
-const host_url = 'http://149.165.154.175';
-module.exports = {host_url};
